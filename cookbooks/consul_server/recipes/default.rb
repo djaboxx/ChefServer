15154a30eefb86479bd93f223114e77877afbd49
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-18 21:17:14 -0700

    adding consul_server cookbook

--- cookbooks/consul_server/recipes/default.rb
+++ cookbooks/consul_server/recipes/default.rb
@@ -5,22 +5,19 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 require 'securerandom'
 
-node[:consul][:packages].each do |pkg_name, pkg|
-	#directory "/mnt/alpha/3p/#{pkg_name}" do
-	#	owner "alpha"
-	#	group "eng"
-	#	mode 0755
-	#end
-	remote_file "/tmp/#{pkg_name}" do
-		source "#{pkg}"
+apt_package "unzip"
+node[:consul][:packages].each do |pkg|
+	remote_file "/tmp/#{pkg[:pkg_name]}" do
+		source "#{pkg[:pkg]}"
 	end
-	execute "unzip /tmp/#{pkg_name}" do
+	execute "unzip /tmp/#{pkg[:pkg_name]}" do
 		cwd "/usr/local/bin"
 	end
 end
 
 node.normal['consul_server']['acl_token'] = SecureRandom.uuid
 
+directory "/etc/consul.d"
 template "/etc/consul.d/consul-default.json" do
 	source "consul-default.json.erb"
 end
@@ -31,6 +28,7 @@ cookbook_file "/usr/local/bin/consul_join.py" do
 	mode 0755
 end
 
+directory "/etc/vault"
 cookbook_file "/etc/vault/vault.conf" do
 	source "vault.conf"
 end
@@ -47,7 +45,4 @@ service "supervisor" do
 	action :enable
 end
 
-
-
-    
-
+execute "echo /usr/local/bin/consul_join.py >> /etc/rc.local"
